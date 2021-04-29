0244b4af57aee15957954432d395ecb1bbaa6957
Dom Golac <domgolac@gmail.com>
2021-04-29 08:45:45 +0000

    create asyncBreeds.js

---
+++ asyncBreeds.js
@@ -0,0 +1,15 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, cb) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) cb(data);
+  });
+};
+
+const printBreed = breed => {
+  console.log('Return Value: ', breed);
+};
+breedDetailsFromFile('Bombay', printBreed);
